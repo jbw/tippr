0a3daa59e96a9b27f826fec7121e9d9c76a81a26
jbw <jbw@users.noreply.github.com>
2021-11-29 22:14:23 +0000

    chore: add api props for swagger

--- src/tippr.api/controllers/create-tip.dto.ts
+++ src/tippr.api/controllers/create-tip.dto.ts
@@ -1,11 +1,15 @@
 import { IsNotEmpty, IsPositive, Min } from "class-validator";
 
+import { ApiBody, ApiProperty } from "@nestjs/swagger";
+
 export class CreateTipDto {
 
   @Min(1)
   @IsPositive()
+  @ApiProperty()
   amount: number;
 
   @IsNotEmpty()
+  @ApiProperty()
   message: string;
 }
